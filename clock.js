4965b4eb61dc2b84b6d38d9b31302717c7430d8c
Gade Himaram <gadehimaram07@gmail.com>
2026-06-17 13:36:57 +0530

    commit

---
+++ clock.js
@@ -0,0 +1,23 @@
+let current_time= '23:59:50';
+let [hours, minutes, seconds]= current_time.split(':').map(Number);
+const clock= document.getElementById('clock');
+
+function clockUpdate(){
+    seconds++;
+    if(seconds===60){
+        seconds=0;
+        minutes++;
+    }
+    if(minutes===60){
+        minutes=0;
+        hours++;
+    }
+    if(hours===24){
+        hours=0;
+    }
+    clock.textContent = String(hours).padStart(2,'0') + ':' +
+                        String(minutes).padStart(2,'0') + ':' +
+                        String(seconds).padStart(2,'0')
+}
+clock.setContent = current_time;
+setInterval(clockUpdate, 1000);
